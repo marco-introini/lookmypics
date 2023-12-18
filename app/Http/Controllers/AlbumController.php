0b3010328625206136c68aee1409cb729972ff24
Marco "Mint" Introini <mint.dev@pm.me>
2023-12-18 12:22:10 +0100

    fix phpstan return types

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -4,16 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\AlbumResource;
 use App\Models\Album;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class AlbumController extends Controller
 {
-    public function index()
+
+    public function index(): AnonymousResourceCollection
     {
         return AlbumResource::collection(Album::all());
     }
 
-    public function store(Request $request)
+    public function store(Request $request): AlbumResource
     {
         $data = $request->validate([
             'name' => ['required'],
@@ -26,12 +30,12 @@ class AlbumController extends Controller
         return new AlbumResource(Album::create($data));
     }
 
-    public function show(Album $album)
+    public function show(Album $album): AlbumResource
     {
         return new AlbumResource($album);
     }
 
-    public function update(Request $request, Album $album)
+    public function update(Request $request, Album $album): AlbumResource
     {
         $data = $request->validate([
             'name' => ['required'],
@@ -46,7 +50,7 @@ class AlbumController extends Controller
         return new AlbumResource($album);
     }
 
-    public function destroy(Album $album)
+    public function destroy(Album $album): JsonResponse
     {
         $album->delete();
 
--- app/Http/Resources/AlbumResource.php
+++ app/Http/Resources/AlbumResource.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /** @mixin \App\Models\Album */
 class AlbumResource extends JsonResource
 {
+    /**
+     * @param  Request  $request
+     * @return array<string, string|Carbon|int|null>
+     */
     public function toArray(Request $request): array
     {
         return [
