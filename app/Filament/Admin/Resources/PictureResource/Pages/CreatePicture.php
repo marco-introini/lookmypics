c49c21cdcac1b4d64e52f91b0b1d79b375e819df
Marco "Mint" Introini <mint.dev@pm.me>
2023-12-22 12:38:35 +0100

    Add PictureResource related pages and configuration
    A set of new pages has been added under the 'PictureResource' namespace, including CreatePicture, EditPicture, and ListPictures. In addition, the configuration for the PictureResource has been established, setting up the form, table, and relations for this resource. This is an important step in expanding the functionality of the picture management system.

---
+++ app/Filament/Admin/Resources/PictureResource/Pages/CreatePicture.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Admin\Resources\PictureResource\Pages;
+
+use App\Filament\Admin\Resources\PictureResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePicture extends CreateRecord
+{
+    protected static string $resource = PictureResource::class;
+}
