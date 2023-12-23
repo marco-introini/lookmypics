30661298e1f7b0d6221e6f2df2443e139bf9b031
Marco "Mint" Introini <mint.dev@pm.me>
2023-12-23 08:28:27 +0100

    Refine Picture model to increase code robustness
    This change updates the Picture model in two main ways. First, it enriches the docblocks for methods that return relationships, providing more clarity about the model's structure. Second, it adds more robust handling for situations where a Picture doesn't have an owning User, ensuring that no errors occur in such cases.

--- app/Models/Picture.php
+++ app/Models/Picture.php
@@ -14,16 +14,20 @@ class Picture extends Model
     protected $guarded = [];
 
     /**
-     * @return BelongsTo
+     * @return BelongsTo<Album,Picture>
      */
     public function album(): BelongsTo
     {
         return $this->belongsTo(Album::class);
     }
 
-    public function ownerUser(): User
+    public function ownerUser(): ?User
     {
-        return User::find($this->album->user->id);
+        if (isset($this->album->user))
+        {
+            return User::find($this->album->user->id);
+        }
+        return null;
     }
 
     public function getUrl(): string
@@ -32,6 +36,10 @@ class Picture extends Model
     }
     public function isOwnedBy(User $user): bool
     {
-        return $this->album->user->id === $user->id;
+        if (isset($this->album->user))
+        {
+            return $this->album->user->id === $user->id;
+        }
+        return false;
     }
 }
