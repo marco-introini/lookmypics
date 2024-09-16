ff17df5d1ec2605c95d5e600ab844a6fa3e3e532
Marco "Mint" Introini <mint.dev@pm.me>
2024-09-16 16:41:46 +0200

    test update

---
+++ tests/Feature/API/APILoginTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\User;
+use function Pest\Laravel\postJson;
+
+test('cannot login with wrong email', function () {
+    $user = User::factory()->create([
+        'email' => 'test@example.com',
+    ]);
+
+    postJson('api/login', [
+        'email' => 'wrong@example.com',
+        'password' => $user->password,
+    ])->assertStatus(401);
+});
+
+test('cannot login with wrong password', function () {
+    $user = User::factory()->create([
+        'password' => bcrypt('password'),
+    ]);
+
+    postJson('api/login', [
+        'email' => $user->email,
+        'password' => 'wrongPassword',
+    ])->assertStatus(401);
+});
+
+test('can login with correct data', function () {
+    $user = User::factory()->create([
+        'email' => 'test@example.com',
+        'password' => 'password',
+    ]);
+
+    postJson('api/login', [
+        'email' => $user->email,
+        'password' => $user->password,
+    ])->assertOk();
+});
