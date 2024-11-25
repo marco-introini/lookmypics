54c7e3956db75593674a365dab7cb4cd612cb3f4
Marco "Mint" Introini <mint.dev@pm.me>
2024-11-25 16:29:07 +0100

    Add Album model and update Picture relation
    Created the Album model with HasFactory and SoftDeletes traits. Updated the Picture model to include a belongsToMany relation with the Album model. Also updated various package versions in composer.lock for dependencies.

--- app/Models/Picture.php
+++ app/Models/Picture.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Picture extends Model
@@ -14,5 +15,10 @@ class Picture extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function albums(): BelongsToMany
+    {
+        return $this->belongsToMany(Album::class);
     }
 }
