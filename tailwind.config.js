4a1e63aa93d9f44e9ce263aa47c923496977a054
Marco "Mint" Introini <mint.dev@pm.me>
2025-01-22 17:03:44 +0100

    Flux styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'selector',
+
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
-        "./resources/**/*.vue",
+        "./vendor/livewire/flux-pro/stubs/**/*.blade.php",
+        "./vendor/livewire/flux/stubs/**/*.blade.php",
     ],
     theme: {
+        fontFamily: {
+            sans: ['Inter', 'sans-serif'],
+        },
         extend: {},
     },
     plugins: [],
