4f56e352ea143895ff64b3dd51bd7c38e7d3e790
Marco "Mint" Introini <mint.dev@pm.me>
2025-01-26 13:48:04 +0100

    Add layout attribute and dynamic title to PictureComponent
    Introduce the `Layout` attribute for a distraction-free layout and bind a `Picture` model to the component. The `render` method now dynamically sets the page title based on the picture's title, enhancing flexibility and UX.

--- app/Livewire/PictureComponent.php
+++ app/Livewire/PictureComponent.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Picture;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
+#[Layout('components.layouts.no-distractions')]
 class PictureComponent extends Component
 {
+    public ?Picture $picture;
+
     public function render()
     {
-        return view('livewire.picture-component');
+        return view('livewire.picture-component')
+            ->title($this->picture?->title ?? 'Picture');
     }
 }
