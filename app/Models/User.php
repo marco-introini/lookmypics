fadf90dd6744d74cc5a21bc21fcd7ed0ff6f164d
Marco "Mint" Introini <mint.dev@pm.me>
2025-02-25 17:08:32 +0100

    Add Rector and Laravel Rector packages, update User model
    Integrated `rector/rector` and `driftingly/rector-laravel` for code refactoring and Laravel-specific upgrades. Updated the `User` model with new properties, role handling, and Filament panel access methods. Adjusted PHP version requirement to `^8.4`.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,20 @@
 
 namespace App\Models;
 
+use App\Enums\UserRole;
+use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Override;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
     /**
@@ -22,6 +27,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -34,16 +40,12 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'role' => UserRole::class,
         ];
     }
 
@@ -54,7 +56,27 @@ class User extends Authenticatable
     {
         return Str::of($this->name)
             ->explode(' ')
-            ->map(fn (string $name) => Str::of($name)->substr(0, 1))
+            ->map(fn(string $name) => Str::of($name)->substr(0, 1))
             ->implode('');
     }
+
+    #[Override]
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return auth()->user()?->isAdmin() ?? false;
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->role == UserRole::ADMIN;
+    }
+
+    public function isActive(): bool
+    {
+        if ($this->role != UserRole::USER) {
+            return false;
+        }
+        return isset($this->email_verified_at);
+    }
+
 }
